9b9afed78082f906f40cff0e8c9a688fb4ad9bb0
Quarzerback <157362104+Quarzerback@users.noreply.github.com>
2024-01-26 16:59:44 +0100

    Change message to ASC II

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,18 @@
 package org.example;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world i did it!");
-
-System.out.println("Hello world i did a test!");
+        try {
+            String asciiArt = FigletFont.convertOneLine("Hello, World!");
+            System.out.println(asciiArt);
+        } catch (IOException e) {
+            // Hier kannst du Code für die Behandlung der IOException hinzufügen
+            e.printStackTrace();
+        }
     }
 
 }
